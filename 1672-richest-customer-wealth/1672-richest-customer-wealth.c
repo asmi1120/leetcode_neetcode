cbc8b038625708287d3ba1e506e20f447ffa1a60
asmi1120 <asmitajeyaprakash123@gmail.com>
2026-01-09 18:56:24 +0530

    Time: 0 ms (100%), Space: 8.8 MB (67.61%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.c
@@ -0,0 +1,12 @@
+int maximumWealth(int** accounts, int accountsSize, int* accountsColSize) {
+    int max =0;
+    for(int i=0;i<accountsSize;i++){
+        int sum=0;
+        for(int j=0;j<accountsColSize[i];j++){
+            sum = sum + accounts[i][j];
+        }
+        if(max<sum)
+        max=sum;
+    }
+    return max;
+}
