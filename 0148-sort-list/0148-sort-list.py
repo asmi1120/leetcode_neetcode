fa36fc15f49165a28d0f0b64e1ccf9c5692b3841
asmi1120 <asmitajeyaprakash123@gmail.com>
2026-06-18 14:19:08 +0530

    Time: 239 ms (11.73%), Space: 40.7 MB (60.98%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -5,16 +5,32 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        arr=[]
-        temp=head
-        while temp!=None :
-            arr.append(temp.val)
-            temp=temp.next
-        arr=sorted(arr)
-        temp=head
-        i=0
-        while temp!=None:
-            temp.val=arr[i]
-            i+=1
-            temp=temp.next
-        return head
+        if head==None or head.next==None:
+            return head
+        middle=self.mid(head)
+        leftnode=head
+        rightnode=middle.next
+        middle.next=None
+        leftnode=self.sortList(leftnode)
+        rightnode=self.sortList(rightnode)
+        return self.sortfn(leftnode,rightnode)
+    def sortfn(self,leftnode,rightnode):
+        cur = dummy = ListNode()
+        while leftnode and rightnode:               
+            if leftnode.val < rightnode.val:
+                cur.next = leftnode
+                leftnode, cur = leftnode.next, leftnode
+            else:
+                cur.next = rightnode
+                rightnode, cur = rightnode.next, rightnode
+                
+        if leftnode or rightnode:
+            cur.next = leftnode if leftnode else rightnode    
+        return dummy.next
+    def mid(self,head):
+        slow=head
+        fast=head.next
+        while fast!=None and fast.next!=None:
+            slow=slow.next
+            fast=fast.next.next
+        return slow
